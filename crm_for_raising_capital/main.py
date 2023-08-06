b8563cf515868187788887b4daf227152365c6b9
Michael Gruen <michaelgruen@Michaels-Mac-Studio.local>
2023-08-06 12:53:13 -0400

    Added IP logging to main.py

--- crm_for_raising_capital/main.py
+++ crm_for_raising_capital/main.py
@@ -1,31 +1,15 @@
+from flask import Flask, request
 import logging
-
-logging.basicConfig(filename='app.log', level=logging.INFO)
 
 app = Flask(__name__)
 
+logging.basicConfig(filename='app.log', level=logging.INFO)
+
 @app.route('/')
-def hello_world():
-    logging.info('Hello, World! route was accessed')
+def home():
+    client_ip = request.remote_addr
+    app.logger.info('Client IP: %s', client_ip)
     return 'Hello, World!'
-
-@app.route('/error')
-def error_route():
-    try:
-        raise Exception('An error occurred!')
-    except Exception as e:
-        logging.error(str(e))
-        return 'An error occurred, check the logs!'
-
-@app.route('/new')
-def new_route():
-    logging.info('New route was accessed')
-    return 'This is a new route!'
-
-@app.route('/trump')
-def trump_route():
-    logging.info('Trump route was accessed')
-    return 'This is the best route, everybody says so!'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
