72a7657790a42f5d69c9c7d26ce833ce83e26def
Michael Gruen <michaelgruen@Michaels-Mac-Studio.local>
2023-08-06 12:51:37 -0400

    Added error logging to main.py

--- crm_for_raising_capital/main.py
+++ crm_for_raising_capital/main.py
@@ -9,5 +9,13 @@ def hello_world():
     logging.info('Hello, World! route was accessed')
     return 'Hello, World!'
 
+@app.route('/error')
+def error_route():
+    try:
+        raise Exception('An error occurred!')
+    except Exception as e:
+        logging.error(str(e))
+        return 'An error occurred, check the logs!'
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
