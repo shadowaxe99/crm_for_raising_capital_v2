218a868ec6c612ce28ade7ba842ab8c52be9792d
Michael Gruen <michaelgruen@Michaels-Mac-Studio.local>
2023-08-06 12:55:15 -0400

    Added input validation to the application

--- crm_for_raising_capital/main.py
+++ crm_for_raising_capital/main.py
@@ -1,23 +1,24 @@
-from flask import Flask, request
+import flask
+from flask import request
 import logging
-from datetime import datetime
 
-app = Flask(__name__)
-
+app = flask.Flask(__name__)
 logging.basicConfig(filename='request.log', level=logging.INFO)
 
-@app.before_request
-def log_request_info():
-    app.logger.info('Headers: %s', request.headers)
-    app.logger.info('Body: %s', request.get_data())
+@app.route('/', methods=['GET'])
+def home():
+    ip = request.remote_addr
+    logging.info(f'IP: {ip}')
+    return {'message': 'Hello, World!'}
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/time')
-def server_time():
-    return {'server_time': datetime.now().isoformat()}
+@app.route('/validate', methods=['POST'])
+def validate():
+    data = request.get_json()
+    if 'username' not in data or 'password' not in data:
+        return {'message': 'Missing username or password'}, 400
+    if len(data['username']) < 5 or len(data['password']) < 8:
+        return {'message': 'Username must be at least 5 characters and password must be at least 8 characters'}, 400
+    return {'message': 'Valid input'}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
